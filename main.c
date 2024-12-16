d172251809bc7df1ba4cebbfbabf028cbc2ff90b
AndreDev <andre.leles.dev@gmail.com>
2024-12-16 02:30:02 -0300

    Tokens structure started

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include "helpers/vector.h"
+#include "compiler.h"
 
 int main()
 {
-    printf("Hello, World.");
+    int res = compile_file("./test.c", ".;test", 0);
+
+    if (res == COMPILER_FILE_COMPILED_OK){
+        printf("everything compilerd fine\n");
+    }else if (res == COMPILER_FAILED_WITH_ERRORS){
+        printf("Unknow response for compile file \n");
+    }
+    
     return 0;
 }
